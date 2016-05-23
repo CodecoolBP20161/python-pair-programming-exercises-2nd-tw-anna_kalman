a8d36cbae14754bb7f1e148d3e2fad3da9387973
annakertesz <kerteszannanak@gmail.com>
2016-05-23 11:52:43 +0200

    fizzbuzz1.1

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,17 @@
 def fizzbuzz(number):
+    if number % 15 == 0:
+        print ("FizzBuzz")
+    elif number % 3 == 0:
+        print ("fizz")
+    elif number % 5 == 0:
+        print ("buzz")
+    else:
+        print (number)
     return
 
-
 def main():
+    for i in range(1, 101):
+        fizzbuzz(i)
     return
 
 if __name__ == '__main__':
