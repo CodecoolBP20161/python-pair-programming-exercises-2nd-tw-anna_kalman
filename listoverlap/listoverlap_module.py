acf75d80824f507d278508a808dc19e491584ae0
annakertesz <kerteszannanak@gmail.com>
2016-05-23 14:06:56 +0200

    with random lists

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,3 +1,12 @@
+from random import randint
+
+def random_list():
+    list_name = []
+    for i in range(1, randint(2, 10)):
+        list_name.append(randint(1, 100))
+    return list_name
+
+
 def listoverlap(list1, list2):
     result_list = []
     main_list = list1 + list2
@@ -11,7 +20,7 @@ def main():
     print (listoverlap(a,b))
     return
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+a = random_list()
+b = random_list()
 if __name__ == '__main__':
     main()
