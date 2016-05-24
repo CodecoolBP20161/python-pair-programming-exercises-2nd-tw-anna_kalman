86b8845b251b0ca51554f912aa02ee048e4e4872
annakertesz <kerteszannanak@gmail.com>
2016-05-24 09:50:40 +0200

    done and done

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,15 @@
 def palindrome(str):
-    return
+    is_palindrome = str.replace(" ", "")
+    if is_palindrome == is_palindrome[::-1]:
+        return "It's a palindrome"
+    else:
+        return "It's not"
 
 
 def main():
-    return
+    str = input("type your str!")
+    print (palindrome(str))
+
 
 
 if __name__ == '__main__':
