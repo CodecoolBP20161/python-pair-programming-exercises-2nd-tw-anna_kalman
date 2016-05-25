ae723a9e630e8914719cdf62ae65339bb7f04d8c
annakertesz <kerteszannanak@gmail.com>
2016-05-25 09:16:41 +0200

    fertig

--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,15 @@ import datetime
 
 
 def years(age):
-    return
+    today = str(datetime.date.today())[:4]
+    return int(today) + 100 - int(age)
 
 
 def main():
-    return
+    name = input("name?")
+    age = input("how old R U?")
+    print ("%s, you'll be 100 in %i" % (name, years(age)))
+
 
 
 if __name__ == '__main__':
